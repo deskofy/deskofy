9d0ebc538be98a754538f8bf1e0352aca9d3712b
las-nish <19279535+las-nish@users.noreply.github.com>
2025-10-05 21:54:45 +0530

    feat: add undefined type to the external path

--- packages/app/src/utils/appPath.ts
+++ packages/app/src/utils/appPath.ts
@@ -1,9 +1,14 @@
 import { app } from 'electron';
 import path from 'path';
 
-const appPath = (externalPath: string | string[]): string =>
-  Array.isArray(externalPath)
+const appPath = (externalPath: string | string[] | undefined): string => {
+  if (externalPath === undefined) {
+    return '';
+  }
+
+  return Array.isArray(externalPath)
     ? path.join(app.getAppPath(), ...externalPath)
     : path.join(app.getAppPath(), externalPath);
+};
 
 export { appPath };
