48ae0f0526178dba03f99b87a04efc787f55d76b
las-nish <19279535+las-nish@users.noreply.github.com>
2025-10-07 16:15:40 +0530

    fix: fix typo

--- packages/cli/src/messages/default.ts
+++ packages/cli/src/messages/default.ts
@@ -16,8 +16,8 @@ const printDefaultMessage = (): void => {
     console.log("Usage: deskofy [command]\n")
     console.log("Try 'deskofy -h' for a list of available commands.\n")
     console.log("Examples:")
-    console.log("  $ deskofy setup  : Setup necessary tools and packages")
-    console.log("  $ deskofy create : Create a deskofy app\n")
+    console.log("  $ deskofy create : Create a deskofy app")
+    console.log("  $ deskofy run    : Run deskofy app\n")
 
     console.log(chalk.yellow("The deskofy is still public preview and not production-ready. Sign up"))
     console.log(chalk.yellow(`for the early access waitlist ${chalk.red('https://deskofy.app')} to receive updates.\n`))
