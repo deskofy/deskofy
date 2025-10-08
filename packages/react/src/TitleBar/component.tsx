82fb3a497527ade8e7f3e2451d8ebbf3691f8e4a
las-nish <19279535+las-nish@users.noreply.github.com>
2025-10-08 23:30:49 +0530

    refactor: optimize component

--- packages/react/src/TitleBar/component.tsx
+++ packages/react/src/TitleBar/component.tsx
@@ -1,11 +1,5 @@
 import React, { CSSProperties, JSX, ReactNode } from 'react';
 
-const Platform = {
-  LINUX: 'LINUX',
-  MAC: 'MAC',
-  WINDOWS: 'WINDOWS',
-} as const;
-
 const ContentAlignment = {
   LEFT: 'LEFT',
   RIGHT: 'RIGHT',
@@ -14,8 +8,6 @@ const ContentAlignment = {
   SPACE_AROUND: 'SPACE_AROUND',
   SPACE_EVENLY: 'SPACE_EVENLY',
 } as const;
-
-type TPlatform = (typeof Platform)[keyof typeof Platform];
 
 type TContentAlignment =
   (typeof ContentAlignment)[keyof typeof ContentAlignment];
@@ -30,8 +22,7 @@ type TComponent = {
 };
 
 type TProps = {
-  platform?: TPlatform;
-  children?: ReactNode;
+  id?: string;
   components?: TComponent[];
   contentAlignment?: TContentAlignment;
   height?: number;
@@ -64,10 +55,12 @@ const TitleBarContainer = (props: TProps): JSX.Element => {
       SPACE_AROUND: 'space-around',
       SPACE_EVENLY: 'space-evenly',
     };
+
     return alignmentMap[contentAlignment];
   };
 
   const containerStyle: CSSProperties = {
+    background: 'red',
     height: `${height}px`,
     paddingLeft: `${inlinePadding}px`,
     paddingRight: `${inlinePadding}px`,
@@ -77,7 +70,10 @@ const TitleBarContainer = (props: TProps): JSX.Element => {
     alignItems: 'center',
     justifyContent: getJustifyContent(),
     width: '100%',
-    position: 'relative',
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    right: 0,
     userSelect: 'none',
     WebkitUserSelect: 'none',
     WebkitAppRegion: isDragable ? 'drag' : 'no-drag',
@@ -111,54 +107,19 @@ const TitleBarContainer = (props: TProps): JSX.Element => {
 };
 
 const TitleBar = (props: TProps): JSX.Element => {
-  const { isVisible = true, children } = props;
+  const { isVisible = true } = props;
 
   if (!isVisible) {
     return <></>;
   }
 
-  const wrapperStyle: CSSProperties = {
-    display: 'flex',
-    flexDirection: 'column',
-    width: '100%',
-    height: '100%',
-  };
-
-  const titleBarStyle: CSSProperties = {
-    flexShrink: 0,
-  };
-
-  const containerStyle: CSSProperties = {
-    flex: 1,
-    overflow: 'auto',
-    position: 'relative',
-  };
-
-  return children ? (
-    <div style={wrapperStyle}>
-      <div style={titleBarStyle}>
-        <TitleBarContainer {...props} />
-      </div>
-      <div style={containerStyle}>{children}</div>
-    </div>
-  ) : (
-    <div style={titleBarStyle}>
-      <TitleBarContainer {...props} />
-    </div>
-  );
+  return <TitleBarContainer {...props} />;
 };
 
 TitleBarContainer.displayName = 'TitleBarContainer';
 
 TitleBar.displayName = 'TitleBar';
 
-export type {
-  TPlatform,
-  TContentAlignment,
-  TComponent,
-  TProps as TTitleBarProps,
-};
+export type { TContentAlignment, TComponent, TProps as TTitleBarProps };
 
-export { Platform, ContentAlignment };
-
-export default TitleBar;
+export { ContentAlignment, TitleBar };
