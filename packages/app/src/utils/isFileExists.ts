f8560aefbe1a7f297945a03fe7ae1d3aa0c602a2
las-nish <19279535+las-nish@users.noreply.github.com>
2025-10-10 01:02:24 +0530

    feat: create isFileExists() fn

---
+++ packages/app/src/utils/isFileExists.ts
@@ -0,0 +1,19 @@
+import fs from 'fs-extra';
+import path from 'path';
+
+const isFileExists = (fileName: string): boolean => {
+  try {
+    const filePath = path.join(process.cwd(), fileName);
+
+    const stat = fs.statSync(filePath);
+    if (stat.isFile()) {
+      return true;
+    }
+  } catch {
+    // Just ignore...
+  }
+
+  return false;
+};
+
+export { isFileExists };
