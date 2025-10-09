4bbca57b4d5e244601746d4031214c2cf37b593e
las-nish <19279535+las-nish@users.noreply.github.com>
2025-10-10 01:03:00 +0530

    fix: fix file checking in isFileExists()

--- packages/cli/src/utils/isFileExists.ts
+++ packages/cli/src/utils/isFileExists.ts
@@ -2,10 +2,15 @@ import fs from 'fs-extra';
 import path from 'path';
 
 const isFileExists = (fileName: string): boolean => {
-  const tsconfigPath = path.join(process.cwd(), fileName);
+  try {
+    const filePath = path.join(process.cwd(), fileName);
 
-  if (!fs.existsSync(tsconfigPath)) {
-    return false;
+    const stat = fs.statSync(filePath);
+    if (stat.isFile()) {
+      return true;
+    }
+  } catch {
+    // Just ignore...
   }
 
   return true;
